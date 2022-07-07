5e88721349e92aca92e54de8408cb8db84030e50
Thaddaus Haase <alu0101016733@ull.edu.es>
2022-07-07 10:02:41 +0100

    added empty conectionVariables file

---
+++ backend/conectionVariables.ts
@@ -0,0 +1,8 @@
+export const TELEGRAM_BOT_TOKEN = 'TOKEN';
+
+export const MQTT_CLIENT_DATA = {
+  port: 1883,
+  host: 'eu1.cloud.thethings.network',
+  username: 'USERNAME',
+  password: 'PASSWORD',
+};
